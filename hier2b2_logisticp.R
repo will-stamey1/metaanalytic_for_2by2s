fd8985d062884a9998e935c66b1d2e2f1dca1884
will-stamey1 <wostamey@gmail.com>
2024-06-16 20:31:31 -0500

    Initial commit for hier2b2_logisticp.R and Poster file. Currently, poster is just an outline from sequential best project.

--- hier2b2_logisticp.R
+++ hier2b2_logisticp.R
@@ -4,19 +4,19 @@ library(rjags)
 # Set seed 
 set.seed(1234)
 
-hier2b2 <- function(iter = 1, nexp = 10, n = 100, p1_alpha = 15, p1_beta = 30, lnRR_mu = 0, 
-                    lnRR_sig = 0.1, prior_p1mu_alpha = 1, prior_p1mu_beta = 1, 
-                    prior_p1rho_alpha = .1, prior_p1rho_beta = .1, prior_lnRRmu_mu = 0, 
+hier2b2_logistp <- function(iter = 1, nexp = 10, n = 100, p1_alpha = 15, p1_beta = 30, lnRR_mu = 0, 
+                    lnRR_sig = 0.1, prior_p1mu_mu = 0, prior_p1mu_tau = .01,
+                    prior_p1_sig_lo = 0, prior_p1_sig_hi = 2, prior_lnRRmu_mu = 0, 
                     prior_lnRRmu_tau = .01, prior_lnRRtau_lo = 0, prior_lnRRtau_hi = 2){
-
   
   # nexp: number of experiments in the simulation.
   # iter: simulation iterations.
   # p1_alpha: True alpha value of the population p1.
-  # p1_beta: True beta value of the population p1. 
-  # lnRR_mu: True mu value of population lnRR. 
-  # lnRR_sig2: True variance of population lnRR.
-  # prior_p1mu_alpha and prior_p1mu_beta: parameters for beta prior on p1
+  # p1_beta: True beta value of the population p1.
+  # lnRR_mu: True mu value of population lnRR.
+  # lnRR_sig: True std. dev. of population lnRR.
+  # prior_p1mu_mu and prior_p1mu_tau: parameters for normal prior on p1s mean, p1mu.
+  # prior_p1_tau_lo and prior_p1_tau_hi: Lower and upper bounds for the uniform prior of the variance of logit(p)
   # prior_lnRRmu_mu and prior_lnRRmu_tau: parameters for normal prior of lnRRmu.
   # prior_lnRRtau_lo and prior_lnRRtau_hi: parameters for uniform prior for lnRRtau.
   
@@ -32,11 +32,9 @@ hier2b2 <- function(iter = 1, nexp = 10, n = 100, p1_alpha = 15, p1_beta = 30, l
   model {
    
     # Prior
-    mu_p ~ dbeta(prior_p1mu_alpha, prior_p1mu_beta)
-    rho_p ~ dgamma(prior_p1rho_alpha, prior_p1rho_beta)
-      
-    p1_alpha <- mu_p*rho_p
-    p1_beta <- rho_p * (1 - mu_p)
+    p1_mu ~ dnorm(prior_p1mu_mu, prior_p1mu_tau)
+    p1_sig ~ dunif(prior_p1_sig_lo, prior_p1_sig_hi)
+    p1_tau <- 1/(p1_sig*p1_sig)  
       
     lnRR_mu ~ dnorm(prior_lnRRmu_mu, prior_lnRRmu_tau)
     lnRR_sig ~ dunif(prior_lnRRtau_lo, prior_lnRRtau_hi)
@@ -50,13 +48,11 @@ hier2b2 <- function(iter = 1, nexp = 10, n = 100, p1_alpha = 15, p1_beta = 30, l
       n1[i] ~ dbin(p1[i], n[i])
       n11[i] ~ dbin(p2[i], n1[i])
       
-      theta1[i] ~ dbeta(p1_alpha, p1_beta)
-      invRR[i] <- 1/RR[i]
-      upbound[i] <- min(1, invRR[i])  
-      p1[i] <- theta1[i] * upbound[i]
+      logitp1[i] ~ dnorm(p1_mu, p1_tau)
+      logit(p1[i]) <- logitp1[i]
       
-      log(RR[i]) <- theta2[i]
-      theta2[i] ~ dnorm(lnRR_mu, lnRR_tau)
+      log(RR[i]) <- theta[i]
+      theta[i] ~ dnorm(lnRR_mu, lnRR_tau)
     }
   }
   
@@ -80,12 +76,12 @@ hier2b2 <- function(iter = 1, nexp = 10, n = 100, p1_alpha = 15, p1_beta = 30, l
   # 12. rhop_covr: coverage of p1 rho true value. . 
   # 13. RRindivcovr: coverage rate for experiment RR means
   # 14. p1indivcovr: coverage rate for experiment p1 means
-  output <- data.frame(matrix(NA, iter, 14))
+  output <- data.frame(matrix(NA, iter, 15))
   colnames(output) <- c("lnRRmu_mean", "lnRRmu_CIlen", "lnRRmu_covr", 
-                        "lnRRtau_mean", "lnRRtau_CIlen", "lnRRtau_covr",
-                        "mup_mean", "mup_CIlen", "mup_covr", 
-                        "rhop_mean", "rhop_CIlen", "rhop_covr",
-                        "RRindivcovr", "p1indivcovr")
+                        "lnRRsig_mean", "lnRRsig_CIlen", "lnRRsig_covr",
+                        "p1mu_mean", "p1mu_CIlen", "p1mu_covr", 
+                        "p1sig_mean", "p1sig_CIlen", "p1sig_covr",
+                        "RRindivcovr", "p1indivcovr", "p1pctbias")
   
   for(i in 1:iter){
     
@@ -101,8 +97,9 @@ hier2b2 <- function(iter = 1, nexp = 10, n = 100, p1_alpha = 15, p1_beta = 30, l
     
     # Define data for JAGS
     data <- list(n = n, nexp = nexp, n1 = n1, n11 = n11, 
-                 prior_p1mu_alpha = prior_p1mu_alpha, prior_p1mu_beta = prior_p1mu_beta, 
-                 prior_p1rho_alpha = prior_p1rho_alpha, prior_p1rho_beta = prior_p1rho_beta,
+                 prior_p1mu_mu = prior_p1mu_mu, 
+                 prior_p1mu_tau = prior_p1mu_tau, prior_p1_sig_lo = prior_p1_sig_lo, 
+                 prior_p1_sig_hi = prior_p1_sig_hi,
                  prior_lnRRmu_mu = prior_lnRRmu_mu, prior_lnRRmu_tau = prior_lnRRmu_tau,
                  prior_lnRRtau_lo = prior_lnRRtau_lo, prior_lnRRtau_hi = prior_lnRRtau_hi)
     
@@ -114,8 +111,8 @@ hier2b2 <- function(iter = 1, nexp = 10, n = 100, p1_alpha = 15, p1_beta = 30, l
     
     
     # iterations
-    samps <- coda.samples(jm, variable.names = c("lnRR_mu", "lnRR_tau", "mu_p", 
-                                                 "rho_p", "p1", "RR"),  
+    samps <- coda.samples(jm, variable.names = c("lnRR_mu", "lnRR_sig", "p1_mu", 
+                                                 "p1_sig", "p1", "RR"),  
                           n.iter = 20000)
     
     stuff <- summary(samps)
@@ -123,31 +120,24 @@ hier2b2 <- function(iter = 1, nexp = 10, n = 100, p1_alpha = 15, p1_beta = 30, l
     output[i,]$lnRRmu_mean <- stuff$statistics["lnRR_mu",1]
     output[i,]$lnRRmu_CIlen <- stuff$quantiles["lnRR_mu",5] - stuff$quantiles["lnRR_mu",1]
     output[i,]$lnRRmu_covr <- stuff$quantiles["lnRR_mu",5] > lnRR_mu & stuff$quantiles["lnRR_mu",1] < lnRR_mu
-    output[i,]$lnRRtau_mean <- stuff$statistics["lnRR_tau",1]
-    output[i,]$lnRRtau_CIlen <- stuff$quantiles["lnRR_tau",5] - stuff$quantiles["lnRR_tau",1]
-    output[i,]$lnRRtau_covr <- stuff$quantiles["lnRR_tau",5] > exp(lnRR_mu) & stuff$quantiles["lnRR_tau",1] < exp(lnRR_mu)
-    output[i,]$mup_mean <- stuff$statistics["mu_p",1]
-    output[i,]$mup_CIlen <- stuff$quantiles["mu_p",5] - stuff$quantiles["mu_p",1]
-    output[i,]$mup_covr <- stuff$quantiles["mu_p",5] > p1_true_mu & stuff$quantiles["mu_p",1] < p1_true_mu
-    output[i,]$rhop_mean <- stuff$statistics["rho_p",1]
-    output[i,]$rhop_CIlen <- stuff$quantiles["rho_p",5] - stuff$quantiles["rho_p",1]
-    output[i,]$rhop_covr <- stuff$quantiles["rho_p",5] > p1_true_rho & stuff$quantiles["rho_p",1] < p1_true_rho
+    output[i,]$lnRRsig_mean <- stuff$statistics["lnRR_sig",1]
+    output[i,]$lnRRsig_CIlen <- stuff$quantiles["lnRR_sig",5] - stuff$quantiles["lnRR_sig",1]
+    output[i,]$lnRRsig_covr <- stuff$quantiles["lnRR_sig",5] > sqrt(lnRR_sig) & stuff$quantiles["lnRR_sig",1] < sqrt(lnRR_sig)
+    output[i,]$p1mu_mean <- stuff$statistics["p1_mu",1]
+    #output[i,]$p1mu_CIlen <- stuff$quantiles["p1_mu",5] - stuff$quantiles["p1_mu",1]
+    #output[i,]$p1mu_covr <- stuff$quantiles["p1_mu",5] > p1_true_mu & stuff$quantiles["p1_mu",1] < p1_true_mu
+    output[i,]$p1sig_mean <- stuff$statistics["p1_sig",1]
+    #output[i,]$p1sig_CIlen <- stuff$quantiles["p1_sig",5] - stuff$quantiles["p1_sig",1]
+    #output[i,]$p1sig_covr <- stuff$quantiles["p1_sig",5] > p1_true_rho & stuff$quantiles["p1_sig",1] < p1_true_rho
     output[i,]$RRindivcovr <- (mean(stuff$quantiles[1:nexp,5] > exp(lnRR) & stuff$quantiles[1:nexp,1] < exp(lnRR)))
-    output[i,]$p1indivcovr <- (mean(stuff$quantiles[nexp + 1:nexp, 5] > p1 & stuff$quantiles[nexp + 1:nexp, 1] < p1))
+    output[i,]$p1indivcovr <- (mean(stuff$quantiles[(nexp + 2) + 1:nexp, 5] > p1 & stuff$quantiles[(nexp + 2) + 1:nexp, 1] < p1))
+    output[i,]$p1pctbias <- mean((p1 - stuff$statistics[grep("p1\\[", rownames(stuff$statistics))])/p1)
   }
-
+  
   return(output)
-
+  
 }
 
 
-hier2b2(iter = 15)
-
-
-
-
-iter = 1; nexp = 10; n = 100; p1_alpha = 15; p1_beta = 30; lnRR_mu = 0 
-lnRR_sig = 0.1; prior_p1mu_alpha = 1; prior_p1mu_beta = 1
-prior_p1rho_alpha = .1; prior_p1rho_beta = .1; prior_lnRRmu_mu = 0
-prior_lnRRmu_tau = .01; prior_lnRRtau_lo = 0; prior_lnRRtau_hi = 2
+hier2b2_logistp(iter = 10)
 
