7810219465ce0a983f34af62e1500a7d81b9a939
will-stamey1 <wostamey@gmail.com>
2024-07-16 19:00:33 -0400

    Added stargazer code to make latex tables.

--- tablesandgraphs/maketables.R
+++ tablesandgraphs/maketables.R
@@ -1,6 +1,7 @@
 # Analyze tablesandgraphs simulation results:
 
 library(dplyr)
+library(stargazer)
 
 a <- read.csv("tablesandgraphs/table1and2job/tblresults.csv")
 
@@ -11,3 +12,44 @@ mean(exp(a$lnRRmu_mean))
 a$mup_mean %>% mean()
 a$lnRRsd_covr %>% mean()
 
+# FORGOT TO ADD LABELS FOR CONFIGURATION PARAMETERS, ADD MANUALLY:
+a$sd <- NA
+a$N <- NA
+
+RRsds <- c(0.05, 0.1, 0.2)
+Ns <- c(30, 100, 300)
+
+i <- 0
+for(sd in RRsds){
+  for(N in Ns){
+    
+    a[(1000*i + 1):(1000*(i + 1)),]$sd <- sd
+    a[(1000*i + 1):(1000*(i + 1)),]$N <- N
+    i <- i + 1
+  }
+}
+
+# Table 1: 
+
+t1 <- 
+  a %>% group_by(sd, N) %>% 
+  summarize(
+    #mean(exp(lnRRmu_mean)),
+    across(lnRRmu_mean:p1avgpctbias, ~mean(.x)))
+
+# Table 2:
+
+t2 <- a %>% 
+  group_by(sd, N) %>% 
+  summarize(
+    across(newRR:newp1CIlen, ~mean(.x))
+  )
+
+
+# Make latex output ###########################################################
+
+t1 %>% mutate(across(everything(), ~round(.x,3))) %>% 
+  stargazer(type = "latex", header = T, summary = F, vars = colnames(t1), colnames = T)
+
+t2 %>% mutate(across(everything(), ~round(.x,3))) %>% 
+  stargazer(type = "latex", header = T, summary = F, vars = colnames(t1), colnames = T)
